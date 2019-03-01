e4e87f8b248a0c21353f96148692ea3271b5941c
lbapp <lbapp@outlook.com>
2019-02-28 23:40:14 -0500

    Added code to switch players and make it possible to fill each cell

--- tictactoe.js
+++ tictactoe.js
@@ -24,6 +24,8 @@ let values = {
   row3col3: 9
 };
 
+let currentPlayer = "X";
+
 const grid = gridValues => {
   return (
     "\n" +
@@ -48,7 +50,9 @@ const grid = gridValues => {
     " | " +
     gridValues.row3col3 +
     "\n\n" +
-    "Player X's turn\n" +
+    "Player " +
+    currentPlayer +
+    "'s turn\n" +
     "Move to?"
   );
 };
@@ -60,6 +64,32 @@ async function start() {
 
   while (prompt.toLowerCase() !== "exit") {
     prompt = await ask(grid(values));
+
+    if (prompt === "1") {
+      values.row1col1 = currentPlayer;
+    } else if (prompt === "2") {
+      values.row1col2 = currentPlayer;
+    } else if (prompt === "3") {
+      values.row1col3 = currentPlayer;
+    } else if (prompt === "4") {
+      values.row2col1 = currentPlayer;
+    } else if (prompt === "5") {
+      values.row2col2 = currentPlayer;
+    } else if (prompt === "6") {
+      values.row2col3 = currentPlayer;
+    } else if (prompt === "7") {
+      values.row3col1 = currentPlayer;
+    } else if (prompt === "8") {
+      values.row3col2 = currentPlayer;
+    } else if (prompt === "9") {
+      values.row3col3 = currentPlayer;
+    }
+
+    if (currentPlayer === "X") {
+      currentPlayer = "O";
+    } else {
+      currentPlayer = "X";
+    }
   }
 
   console.log("Thanks for playing!");
